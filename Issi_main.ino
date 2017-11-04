36a943a532e6beba09c023872d11c604c2469772
woot835 <mhass@itcollege.ee>
2017-11-04 20:22:20 +0200

    Add files via upload
    V1, Nuppude süsteem implementeeritud, pean leidma viisi vastus variable välja saata ja saada START booleani kusagilt.

---
+++ Issi_main.ino
@@ -0,0 +1,65 @@
+#include <Keypad.h>
+
+const byte ROWS = 3; //kolm rida
+const byte COLS = 1; //uks tulp
+boolean FLAG = 1;
+boolean START = 1;
+char vastus;
+char keys[ROWS][COLS] = {
+  {'P'},
+  {'V'},
+  {'E'}
+};
+
+byte rowPins[ROWS] = {9, 10, 11}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5}; //connect to the column pinouts of the keypad
+int ledPin = 2;
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(ledPin, HIGH); // Roheline led pin D2, punane led D3
+  //delay(500);
+  //digitalWrite(ledPin, LOW);
+  //delay(500);
+
+  char key = keypad.getKey(); // Saab valiku kätte
+
+  while (key && FLAG == 1) // Hakkab tööle ainult kui voti ja FLAG on olemas {
+    if (key == 'P') {
+      Serial.println("Poolt");
+      FLAG = 0;
+      digitalWrite(ledPin, LOW);
+      ledPin = 3;
+      vastus = key;
+
+    } else if (key == 'V') {
+      Serial.println("Vastu");
+      FLAG = 0;
+      digitalWrite(ledPin, LOW);
+      ledPin = 3;
+      vastus = key;
+
+    } else if (key == 'E') {
+      Serial.println("Erapooletu");
+      FLAG = 0;
+      digitalWrite(ledPin, LOW);
+      ledPin = 3;
+      vastus = key;
+
+    } else {
+      FLAG = 0;
+      digitalWrite(ledPin, LOW);
+      ledPin = 3;
+      vastus = key;
+
+    }
+    break;
+  }
+
+}
